384e771780a1cf8c68479226480264ac2c42c603
Matt Eric <mf9646a@alumni.american.edu>
2020-11-23 19:10:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Questions from "Cracking the Coding Interview 6th Edition" by Gayle Laakmann McDowell
+
+Use `npm run start` to start the development server
---
+++ index.js
@@ -0,0 +1,58 @@
+import questions from './questions'
+
+const pre = document.querySelector('pre')
+const select = document.querySelector('select')
+const button = document.querySelector('button')
+const instructions = document.getElementById('instructions')
+const showResults = document.getElementById('results')
+const showExpectations = document.getElementById('expectations')
+const outcome = document.getElementById('outcome')
+
+select.addEventListener( "change", ( e ) => {
+    handleSelectChange(e.target.value)
+});
+
+button.addEventListener( "click", ( e ) => {
+    getSolution(select.value)
+});
+
+const runMethodWithTestCases = ( question ) => {
+    const { method, tests } = question;
+    const { inputs, expectedOutputs } = tests;
+    const results = []
+    for(let i=0; i<inputs.length; i++){
+        results.push( method(inputs[i]) )
+    }
+    const stringifiedResults = JSON.stringify( results )
+    const stringifiedExpectations = JSON.stringify( expectedOutputs )
+    showResults.innerHTML = `${stringifiedResults} (results)`
+    showExpectations.innerHTML = `${stringifiedExpectations} (expectations)`
+    outcome.innerHTML = stringifiedResults !== stringifiedExpectations ? "Failed" : "Passed"
+}
+
+const handleSelectChange = ( value ) => {
+    let questionObject = getCurrentQuestionObject(value)
+    setDisplayInfo(questionObject)
+}
+
+const setDisplayInfo = ( question ) => {
+    instructions.innerHTML = question ? question.instructions : null
+    pre.innerText = question ? question.method : null
+}
+
+const getSolution = ( value ) => {
+    const questionObject = getCurrentQuestionObject(value)
+    runMethodWithTestCases( questionObject )
+}
+
+const getCurrentQuestionObject = ( displayName ) => {
+    return questions.find(q => q.displayName === displayName)
+}
+
+for(let i=0; i<questions.length; i++){
+    const name = questions[i].displayName
+    const option = document.createElement('option')
+    option.value = name
+    option.innerText = name
+    select.appendChild(option)
+}
