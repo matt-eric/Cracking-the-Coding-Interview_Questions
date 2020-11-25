2a62e34e5b728f615af40d371efbe0107083feb1
Matt Eric <mf9646a@alumni.american.edu>
2020-11-25 16:49:51 -0500

    Close #6 One Away

--- questions/index.js
+++ questions/index.js
@@ -2,12 +2,14 @@ import isUnique from './src/1_Is-Unique'
 import checkPermutation from './src/2_Check-Permutation'
 import URLify from './src/3_URLify'
 import palindromePermutation from './src/4_Palindrome-Permutation'
+import oneAway from './src/5_One-Away'
 
 const questions = [ 
     isUnique,
     checkPermutation,
     URLify,
-    palindromePermutation
+    palindromePermutation,
+    oneAway
 ]
 
 export default questions
---
+++ questions/src/5_One-Away/index.js
@@ -0,0 +1,60 @@
+const displayName = "One Away"
+
+const instructions = "There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away."
+
+//Possible situations that would constitute an edit:
+//Adding/Removing a character to one string would make the two strings equal
+//Changing a character in one string would make the two strings equal
+
+const method = ( strings ) => {
+    if(strings[0] !== strings[1]) {
+
+        const tryChangingACharacter = ( stringOne, stringTwo ) => {
+            let changeHasBeenMade = false
+            for(let i=0; i<stringOne.length; i++){
+                if( stringOne[i] !== stringTwo[i] ) {
+                    if (changeHasBeenMade) { return false } else { changeHasBeenMade = true }
+                }
+            }
+            return true
+        }
+
+        const tryAddingOrRemovingACharacter = ( stringOne, stringTwo ) => {
+            const canCharacterBeAddedOrRemoved = ( firstString, secondString, index ) => {
+                const stringOneWithInsertedCharacter = firstString.slice(0, index) + secondString[index] + firstString.slice(index);
+                const stringTwoWithInsertedCharacter = secondString.slice(0, index) + firstString[index] + secondString.slice(index);
+                if( (stringOneWithInsertedCharacter === secondString) || (stringTwoWithInsertedCharacter === firstString) ) { return true } else { return false }
+            }
+            for(let i=0; i<stringOne.length; i++){
+                if( stringOne[i] !== stringTwo[i] ) {
+                    return canCharacterBeAddedOrRemoved( stringOne, stringTwo, i )
+                }
+            }
+            return true
+        }
+
+        if(strings[0].length === strings[1].length){
+            return tryChangingACharacter(strings[0], strings[1])
+        } else if (strings[0].length === strings[1].length+1 || strings[0].length === strings[1].length-1){
+            return tryAddingOrRemovingACharacter(strings[0], strings[1])
+        } else {
+            return false
+        }
+        
+    }
+    return true
+}
+
+const tests = {
+    inputs: [ [ 'hello', 'hllo' ], [ 'hellos', 'hello' ], [ 'world', 'wirld' ], [ 'hello', 'world' ] ],
+    expectedOutputs: [ true, true, true, false ]
+}
+
+const oneAway = {
+    displayName,
+    instructions,
+    method,
+    tests
+}
+
+export default oneAway
