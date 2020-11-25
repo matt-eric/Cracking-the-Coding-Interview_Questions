4933200358b04a9e194d2c0711821796ce5447a6
Matt Eric <mf9646a@alumni.american.edu>
2020-11-25 13:50:07 -0500

    Close #4 URLify

--- questions/index.js
+++ questions/index.js
@@ -1,9 +1,11 @@
 import isUnique from './src/1_Is-Unique'
 import checkPermutation from './src/2_Check-Permutation'
+import URLify from './src/3_URLify'
 
 const questions = [ 
     isUnique,
-    checkPermutation
+    checkPermutation,
+    URLify
 ]
 
 export default questions
