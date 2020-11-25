6c89b74771eeb0e520e1b609e5872f72f6e5591f
Matt Eric <mf9646a@alumni.american.edu>
2020-11-25 14:08:44 -0500

    Close #5 Palindrome Permutation

--- questions/index.js
+++ questions/index.js
@@ -1,11 +1,13 @@
 import isUnique from './src/1_Is-Unique'
 import checkPermutation from './src/2_Check-Permutation'
 import URLify from './src/3_URLify'
+import palindromePermutation from './src/4_Palindrome-Permutation'
 
 const questions = [ 
     isUnique,
     checkPermutation,
-    URLify
+    URLify,
+    palindromePermutation
 ]
 
 export default questions
