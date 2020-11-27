37e42d0c00de0b75b737ac4cd434cf1a055ce255
Matt Eric <mf9646a@alumni.american.edu>
2020-11-27 17:13:45 -0500

    Close #28 Delete Middle Node

--- chapters/2-Linked-Lists/index.js
+++ chapters/2-Linked-Lists/index.js
@@ -1,9 +1,11 @@
 import removeDups from './1_Remove-Dups'
 import returnKthToLast from './2_Return-Kth-to-Last'
+import deleteMiddleNode from './3_Delete-Middle-Node'
 
 const linkedLists = [ 
     removeDups,
-    returnKthToLast
+    returnKthToLast,
+    deleteMiddleNode
 ]
 
 export default linkedLists
