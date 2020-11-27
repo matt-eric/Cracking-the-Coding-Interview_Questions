80c25ddd60e62736ab1b8eefbb60b915b694c840
Matt Eric <mf9646a@alumni.american.edu>
2020-11-27 12:50:10 -0500

    Fix #36 Bundle questions into chapter directories

---
+++ chapters/1-Arrays-And-Strings/index.js
@@ -0,0 +1,23 @@
+import isUnique from './1_Is-Unique'
+import checkPermutation from './2_Check-Permutation'
+import URLify from './3_URLify'
+import palindromePermutation from './4_Palindrome-Permutation'
+import oneAway from './5_One-Away'
+import stringCompression from './6_String-Compression'
+import rotateMatrix from './7_Rotate-Matrix'
+import zeroMatrix from './8_Zero-Matrix'
+import stringRotation from './9_String-Rotation'
+
+const arraysAndStrings = [ 
+    isUnique,
+    checkPermutation,
+    URLify,
+    palindromePermutation,
+    oneAway,
+    stringCompression,
+    rotateMatrix,
+    zeroMatrix,
+    stringRotation
+]
+
+export default arraysAndStrings
