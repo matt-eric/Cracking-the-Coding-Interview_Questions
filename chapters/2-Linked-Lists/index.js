0a120c7c07f435871d2b4b707df4a3d0585627fe
Matt Eric <mf9646a@alumni.american.edu>
2020-11-27 16:15:29 -0500

    Close #27 Return Kth to Last

--- chapters/2-Linked-Lists/index.js
+++ chapters/2-Linked-Lists/index.js
@@ -1,7 +1,9 @@
 import removeDups from './1_Remove-Dups'
+import returnKthToLast from './2_Return-Kth-to-Last'
 
 const linkedLists = [ 
     removeDups,
+    returnKthToLast
 ]
 
 export default linkedLists
