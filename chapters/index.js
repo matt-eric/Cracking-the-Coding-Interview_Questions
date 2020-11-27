1ff4941a03521d00981df1ed2a7479a597981616
Matt Eric <mf9646a@alumni.american.edu>
2020-11-27 14:57:23 -0500

    Add linkedList chapter

--- chapters/index.js
+++ chapters/index.js
@@ -1,9 +1,14 @@
 import arraysAndStrings from './1-Arrays-And-Strings'
+import linkedLists from './2-Linked-Lists'
 
 const chapters = [ 
     {
         displayName: "Arrays and Strings",
         questions: arraysAndStrings
+    },
+    {
+        displayName: "Linked Lists",
+        questions: linkedLists
     },
 ]
 
