86ea01c2916bc0239579f0c7ce3dc0ca780d5ba2
Matt Eric <mf9646a@alumni.american.edu>
2020-12-13 22:42:21 -0500

    Add closure (#67)

--- chapters/1-Arrays-And-Strings/9_String-Rotation/index.js
+++ chapters/1-Arrays-And-Strings/9_String-Rotation/index.js
@@ -1,43 +1,42 @@
-const displayName = "String Rotation"
+const stringRotation = {};
 
-const instructions = "Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, 51 and 52, write code to check if 52 is a rotation of 51 using only one call to isSubstring (e.g., 'waterbottle' is a rotation of 'erbottlewat')."
+(() => {
 
-const method = ( strings ) => {
-    const stringOne = strings[0];
-    let stringTwo = strings[1];
-    if(stringOne.length === stringTwo.length){
-        for(let i=0; i<stringOne.length; i++){
-            let stringWithRemovedCharacter = stringTwo.replace(stringOne[i], "")
-            if (stringTwo.length === stringWithRemovedCharacter.length){
-                break;
-            } else {
-                stringTwo = stringWithRemovedCharacter;
-            }
-        }  
-    }
-    return !stringTwo.length
-}
+    stringRotation.displayName = "String Rotation";
 
-const tests = {
-    inputs: [ 
-        ['waterbottle', 'erbottlewat'],
-        ['aterbottle', 'waterbottle'],
-        ['waterbottled', 'waterbottle'],
-        ['watrbottlez', 'waterbottle'],
-     ],
-    expectedOutputs: [ 
-        true, 
-        false ,
-        false,
-        false
-    ],
-}
+    stringRotation.instructions = "Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, 51 and 52, write code to check if 52 is a rotation of 51 using only one call to isSubstring (e.g., 'waterbottle' is a rotation of 'erbottlewat').";
+    
+    stringRotation.method = ( strings ) => {
+        const stringOne = strings[0];
+        let stringTwo = strings[1];
+        if (stringOne.length === stringTwo.length) {
+            for (let i = 0; i < stringOne.length; i++) {
+                let stringWithRemovedCharacter = stringTwo.replace(stringOne[i], "");
+                if (stringTwo.length === stringWithRemovedCharacter.length) {
+                    break;
+                } else {
+                    stringTwo = stringWithRemovedCharacter;
+                };
+            };
+        };
+        return !stringTwo.length;
+    };
+    
+    stringRotation.tests = {
+        inputs: [ 
+            ['waterbottle', 'erbottlewat'],
+            ['aterbottle', 'waterbottle'],
+            ['waterbottled', 'waterbottle'],
+            ['watrbottlez', 'waterbottle'],
+         ],
+        expectedOutputs: [ 
+            true, 
+            false ,
+            false,
+            false,
+        ],
+    };
 
-const zeroMatrix = {
-    displayName,
-    instructions,
-    method,
-    tests
-}
+})();
 
-export default zeroMatrix
+export default stringRotation;
