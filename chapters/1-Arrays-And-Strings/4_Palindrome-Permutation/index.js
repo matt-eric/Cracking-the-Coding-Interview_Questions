806ff9fba8c2a70a4673b4c62687dc8578d8f0c5
Matt Eric <mf9646a@alumni.american.edu>
2020-12-13 22:42:03 -0500

    Add closure (#67)

--- chapters/1-Arrays-And-Strings/4_Palindrome-Permutation/index.js
+++ chapters/1-Arrays-And-Strings/4_Palindrome-Permutation/index.js
@@ -1,26 +1,25 @@
-const displayName = "Palindrome Permutation"
+const palindromePermutation = {};
 
-const instructions = "Given a string, write a function to check if it is a permutation of a palin- drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.The palindrome does not need to be limited to just dictionary words."
+(() => {
 
-const method = ( string ) => {
-    for(let i=0; i<string.length/2; i++){
-        let firstLetter = string[i]
-        let lastLetter = string[string.length-i-1]
-        if ( firstLetter !== lastLetter ) return false
-    }
-    return true
-}
+    palindromePermutation.displayName = "Palindrome Permutation";
 
-const tests = {
-    inputs: [ "abcdedcba", "use a whiteboard draobetihw a esu", "This is not a palindrome." ],
-    expectedOutputs: [ true, true, false ]
-}
+    palindromePermutation.instructions = "Given a string, write a function to check if it is a permutation of a palin- drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters.The palindrome does not need to be limited to just dictionary words.";
 
-const palindromePermutation = {
-    displayName,
-    instructions,
-    method,
-    tests
-}
+    palindromePermutation.method = ( string ) => {
+        for (let i = 0; i < string.length/2; i++) {
+            let firstLetter = string[i];
+            let lastLetter = string[string.length-i-1];
+            if ( firstLetter !== lastLetter ) return false;
+        };
+        return true;
+    };
 
-export default palindromePermutation
+    palindromePermutation.tests = {
+        inputs: [ "abcdedcba", "use a whiteboard draobetihw a esu", "This is not a palindrome." ],
+        expectedOutputs: [ true, true, false ]
+    };
+
+})();
+
+export default palindromePermutation;
